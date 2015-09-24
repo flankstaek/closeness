ffdde8abe1bd1d77ad0e4e0178a9217974c2cfb2
Ian Swain <iswain@applause.com>
2015-09-24 17:05:38 -0400

    Ch-ch-ch-change

--- src/Collider.hx
+++ src/Collider.hx
@@ -1,5 +1,6 @@
 import luxe.Component;
 import luxe.Visual;
+import luxe.Vector;
 
 class Collider extends Component {
 
@@ -7,10 +8,27 @@ class Collider extends Component {
   private var geo : phoenix.geometry.CircleGeometry;
   public var collider : luxe.collision.shapes.Circle;
 
+  public var Position : Vector;
+  public var Radius : Float;
+
   override function init() {
     vis = cast entity;
     geo = cast vis.geometry;
 
-    collider = new luxe.collision.shapes.Circle(geo.);
+    trace(Position);
+
+    collider = new luxe.collision.shapes.Circle(Position.x, Position.y, Radius);
   }
+
+  public function new ( _Position : Vector, _Radius : Float ) {
+    Position = _Position;
+    Radius = _Radius;
+
+    super({ name : "collider" });
+  }
+
+  override function update ( dt : Float ) {
+    collider.position.set_xy(Position.x, Position.y);
+  }
+
 }
--- src/Main.hx
+++ src/Main.hx
@@ -5,28 +5,53 @@ import luxe.Color;
 
 class Main extends luxe.Game {
 
-  var m : Visual;
+  var m : Visual; //Mom
+  var y : Visual; //You
+
+  var rm : Float;
+  var ry : Float;
+
+  var cm : Collider;
+  var cy : Collider;
 
   override function ready() {
+    rm = 200;
+    ry = 20;
+
     m = new Visual({
       pos : Luxe.screen.mid,
       geometry : Luxe.draw.circle({
         color : new Color(1, 1, 1),
-        r : 20,
+        r : rm,
       }),
     });
-    m.add(new Collider());
-  }
+    cm = new Collider(m.pos, rm);
+    m.add(cm);
 
-  override function onkeyup( e:KeyEvent ) {
-    if(e.keycode == Key.escape) {
-        Luxe.shutdown();
-    }
-
+    y = new Visual({
+      pos : Luxe.screen.cursor.pos,
+      geometry : Luxe.draw.circle({
+        color : new Color(1, 1, 1),
+        r : ry
+      }),
+    });
+    cy = new Collider(y.pos, ry);
+    y.add(cy);
   }
 
   override function update(dt:Float) {
+    y.pos = Luxe.screen.cursor.pos;
+    cy.Position = y.pos;
 
+    if(luxe.collision.Collision.shapeWithShape(cy.collider, cm.collider) != null) {
+      trace('bang');
+    }
+  }
+
+  override function onkeyup( event : luxe.KeyEvent) {
+    if(event.keycode == Key.space) {
+      //scale
+    }
   }
 
 
